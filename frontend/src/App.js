2c688c3d6fc8276614dc22067330ba18d20e80d4
nirajsub <78416265+nirajsub@users.noreply.github.com>
2023-05-25 23:34:19 +0545

    starting react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,9 @@
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
 
 export default function App() {
   return (
     <>
-    <Home />
-    <Header />
+  <h1>First React App!</h1>
     </>
   )
 }
