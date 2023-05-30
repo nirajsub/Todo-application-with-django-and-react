75403f4755b8601ee2ddb7bc41e39b50dec066d2
nirajsub <78416265+nirajsub@users.noreply.github.com>
2023-05-30 23:19:27 +0545

    add counter

--- frontend/src/components/Meme.jsx
+++ frontend/src/components/Meme.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import MemesData from "./MemesData";
 
-const Meme = () => {
+
+
+function Meme() {
+    const memesArray = MemesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    const url = memesArray[randomNumber].url
+    console.log(url);
+
+
+    const [counter, setCounter] = useState(0)
+    function sub() {
+        setCounter(prevCount => prevCount - 1)
+    }
+    function add() {
+        setCounter(prevCount => prevCount + 1)
+    }
   return (
     <main>
       <form>
+        <div>
+        <div className="flex flex-col justify-center items-center">
+            <div>
+                <h1 className="font-bold text-slate-800 text-2xl mb-5">Relax! This is just a counter</h1>
+            </div>
+            <div className="counter">
+            <button type="button" className="counter--minus" onClick={sub}>-</button>
+            <div className="counter--count">
+                <h1>{counter}</h1>
+            </div>
+            <button type="button" className="counter--plus" onClick={add}>+</button>
+            </div>
+        </div>
+        </div>
         <div className="flex justify-center items-center space-x-16 my-6">
           <div className="flex flex-col">
             <label htmlFor="top" className="font-bold text-sm mb-2">
@@ -32,9 +62,13 @@ const Meme = () => {
           <button
             type="button"
             className="bg-gradient-to-r from-purple-800 to-pink-600 text-white py-2 px-4 rounded-md"
+            onClick={Meme}
           >
             Get a new meme
           </button>
+        </div>
+        <div className="flex justify-center">
+            <img src={url}/>
         </div>
       </form>
     </main>
