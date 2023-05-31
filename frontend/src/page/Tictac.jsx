5e538e5bd970717c407f698bd382f9bdb7126791
nirajsub <78416265+nirajsub@users.noreply.github.com>
2023-05-31 12:53:33 +0545

    tic tac toe

--- frontend/src/page/Tictac.jsx
+++ frontend/src/page/Tictac.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Tictac = () => {
   return (
@@ -7,40 +7,84 @@ const Tictac = () => {
     </div>
   );
 };
+
 function Board() {
-    
+    const [xIsNext, setXIsNext] = useState(true);
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    const [wiwinner, setWiwinner] = useState()
+    function handleClick(i) {
+      calculateWinner(squares)
+      if (squares[i] || wiwinner) {
+        return;
+      }
+      const nextSquares = squares.slice();
+      if (xIsNext) {
+        nextSquares[i] = "X";
+      } else {
+        nextSquares[i] = "O";
+      }
+      setSquares(nextSquares)
+      setXIsNext(!xIsNext);
+  }
+  
+  const winner = calculateWinner(squares)
+  useEffect(() => {
+    setWiwinner(winner)
+  },[winner])
+
   return (
     <>
+    <div>
+       {wiwinner && <h3>Winner is Mr. : {wiwinner}</h3>}
+    </div>
       <div className="board-row">
-        <Square />
-        <Square/>
-        <Square/>
+        <Square value={squares[0]} onSquareClick={ () => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={ () => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={ () => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[3]} onSquareClick={ () => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={ () => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={ () => handleClick(5)}/>
       </div>
       <div className="board-row">
-        <Square/>
-        <Square/>
-        <Square/>
+        <Square value={squares[6]} onSquareClick={ () => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={ () => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={ () => handleClick(8)}/>
       </div>
     </>
   );
 }
-function Square() {
-    const [value, setValue] = useState(null);
 
-    function handleClick() {
-        setValue('X')
-    }
+function Square({value, onSquareClick}) {
+    // const [value, setValue] = useState(null);
+
+    
   return <button 
   className="square w-20 h-20 border-2 border-sky-300 text-3xl"
-  onClick={handleClick}
+  onClick={onSquareClick}
   >
     {value}
     </button>;
+}
+
+function calculateWinner(squares) {
+  const list = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+  ]
+  for (let i=0; i<list.length; i++){
+    const [a,b,c] = list[i];
+    if (squares[a] && squares [a] === squares[b] && squares[a] === squares[c]){
+      return squares[a]
+    }
+  }
 }
 
 export default Tictac;
